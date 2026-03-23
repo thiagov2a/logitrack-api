55167a5c84b479f7a1d8d206144ef30d30ca0215
Pablo Rodriguez <pablorodriguez1095@gmail.com>
2026-03-23 02:15:39 -0300

    feat(tracking): US-13-visualizacion-de-detalle-completo-de-envio

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #from fastapi import FastAPI
+#from models.tracking import router as tracking_router
 
 #app = FastAPI(
-#    title="LogiTrack API",
- #   description="Mock API para el MVP de Logística",
-  #  version="1.0.0"
+ #   title="LogiTrack API",
+  #  description="Mock API para el MVP de Logística",
+   # version="1.0.0"
 #)
+
+#app.include_router(tracking_router)
 
 #@app.get("/")
 #def estado_del_servidor():
  #   return {"mensaje": "¡El servidor de LogiTrack está vivo y funcionando!", "estado": "OK"}
  
+
+# main.py
 from fastapi import FastAPI
 from models.tracking import router as tracking_router
 
